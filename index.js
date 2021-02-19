681f420e34c1f046012ba1e8bbdda9cb1e3ab361
abhimanyu_mtx <abhimanyu.tanwar@mtxb2b.com>
2021-02-19 10:02:12 +0530

    test

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/:id', function(request, response) {
   response.send('Hello World!')
+})
+
+app.get('/multiply/', function(request, response) {
+  response.send(req.query.num1* req.query.num3);
 })
 
 app.listen(app.get('port'), function() {
